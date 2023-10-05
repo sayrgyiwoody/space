61f09485d062d2a6e30213ae7e5f13e58be0109a
Woody <waiyanwoody@gmail.com>
2023-10-05 12:21:04 +0630

    only readme left

--- asset/js/detail.js
+++ asset/js/detail.js
@@ -42,7 +42,7 @@ function renderPost(post){
               </a>
   
               <ul class="dropdown-menu">
-                  <li><a href="../view/edit.html?id=${post.id}" class="dropdown-item" ><svg class="me-2" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M6 22q-.825 0-1.413-.588T4 20V4q0-.825.588-1.413T6 2h8l6 6v3.1l-8 7.975V22H6Zm8 0v-2.125l5.15-5.175l2.15 2.1l-5.175 5.2H14Zm8.025-5.9L19.9 13.975l.7-.7q.3-.3.725-.3t.7.3l.7.725q.275.3.275.713t-.275.687l-.7.7ZM13 9h5l-5-5v5Z"/></svg><span class="fw-semibold">Edit</span></a></li>
+                  <li><button onclick="editPost(${post.id})" href="" class="dropdown-item" ><svg class="me-2" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M6 22q-.825 0-1.413-.588T4 20V4q0-.825.588-1.413T6 2h8l6 6v3.1l-8 7.975V22H6Zm8 0v-2.125l5.15-5.175l2.15 2.1l-5.175 5.2H14Zm8.025-5.9L19.9 13.975l.7-.7q.3-.3.725-.3t.7.3l.7.725q.275.3.275.713t-.275.687l-.7.7ZM13 9h5l-5-5v5Z"/></svg><span class="fw-semibold">Edit</span></button></li>
                   <li><button onclick="deletePost(${post.id})" class="dropdown-item" ><svg class="me-2" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M9 17h2V8H9v9Zm4 0h2V8h-2v9Zm-8 4V6H4V4h5V3h6v1h5v2h-1v15H5Z"/></svg><span class="fw-semibold">Delete</span></button></li>
               </ul>
           </div>
@@ -121,6 +121,27 @@ function renderPost(post){
 
     }
 
+// direct to edit post 
+function editPost(id){
+  if(checkAuthToken()){
+    window.location = `../view/edit.html?id=${id}`;
+  }else {
+    Swal.fire({
+      imageUrl: '../asset/images/login-astro.png',
+          title: 'Login Required',
+          imageHeight : 200,
+          text : 'Please login first to use this feature.',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Go to Login'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        window.location = "./login.html"
+      }
+    })
+  }
+}
 
       // check auth token from localStorage 
       function checkAuthToken(){
